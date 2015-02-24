55591edb9dfd7e647e49b50f3d58bd39367024b1
Tobias Hornberger <saibot1207@googlemail.com>
2015-02-24 13:05:23 +0100

    Work on aesthetics

--- app/src/main/java/com/example/saibot1207/tobiasapp/TabBar.java
+++ app/src/main/java/com/example/saibot1207/tobiasapp/TabBar.java
@@ -56,8 +56,8 @@ public class TabBar extends TabActivity implements OnTabChangeListener{
         tabHost.getTabWidget().getChildAt(0).setBackgroundColor(-7829368);
         tabHost.getTabWidget().getChildAt(2).setBackgroundColor(-7829368);
 
-        tabHost.getTabWidget().setCurrentTab(2);
-        //tabHost.getTabWidget().getChildAt(1).setBackgroundColor(-1);
+        tabHost.setCurrentTab(1);
+        //tabHost.getTabWidget().getChildAt(2).setBackgroundColor(-1);
 
 
     }
